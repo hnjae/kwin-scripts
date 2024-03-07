22f007e4fff8fbe0dee19df60df800265d711c96
KIM Hyunjae <hnjae@outlook.com>
2024-03-07 15:22:27 +0900

    refactor: merge `update` and `bind` function

--- virtual-desktops-only-on-primary/contents/code/main.js
+++ virtual-desktops-only-on-primary/contents/code/main.js
@@ -18,47 +18,46 @@ function updatePrimaryScreen() {
 }
 
 function bind(window) {
+    var update = function (window) {
+        var window = window || this;
+
+        if (
+            window.desktopWindow ||
+            window.dock ||
+            (!window.normalWindow && window.skipTaskbar)
+        ) {
+            return;
+        }
+
+        var currentScreen = window.screen;
+        var previousScreen = window.previousScreen;
+        window.previousScreen = currentScreen;
+
+        if (currentScreen != primaryScreen) {
+            window.desktop = -1;
+            print("Window " + window.windowId + " has been pinned");
+        } else if (previousScreen != primaryScreen) {
+            window.desktop = workspace.currentDesktop;
+            print("Window " + window.windowId + " has been unpinned");
+        }
+    };
+
     window.previousScreen = window.screen;
+
+    update(window);
+
     window.screenChanged.connect(window, update);
     window.desktopChanged.connect(window, update);
     print("Window " + window.windowId + " has been bound");
-}
-
-function update(window) {
-    var window = window || this;
-
-    if (
-        window.desktopWindow ||
-        window.dock ||
-        (!window.normalWindow && window.skipTaskbar)
-    ) {
-        return;
-    }
-
-    var currentScreen = window.screen;
-    var previousScreen = window.previousScreen;
-    window.previousScreen = currentScreen;
-
-    if (currentScreen != primaryScreen) {
-        window.desktop = -1;
-        print("Window " + window.windowId + " has been pinned");
-    } else if (previousScreen != primaryScreen) {
-        window.desktop = workspace.currentDesktop;
-        print("Window " + window.windowId + " has been unpinned");
-    }
-}
-
-function bindUpdate(window) {
-    bind(window);
-    update(window);
 }
 
 function main() {
     updatePrimaryScreen();
 
     workspace.clientList().forEach(bind);
-    workspace.clientList().forEach(update);
-    workspace.clientAdded.connect(bindUpdate);
+    workspace.clientAdded.connect(bind);
+
+    // TODO: what if primary screen changes? <2024-03-07>
 }
 
 main();
